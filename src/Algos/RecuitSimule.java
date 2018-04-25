a31d62d53e8fc5c787b743ac92687c2729e3fedb
Mark Kpamy <markkpamy@gmail.com>
2018-04-25 23:33:54 +0200

    ajout librairies

--- src/Algos/RecuitSimule.java
+++ src/Algos/RecuitSimule.java
@@ -9,32 +9,33 @@ public class RecuitSimule {
 
     //paramètres de l'algorithme de recuit simulé
     private Solution solutionInitiale;
-    private float tempInitiale;
+    private Double tempInitiale;
     //fonction de decroissance de la temperature
-    public float decreaseFunction(float mu){
-
-
-        return 0;
+    public Double decreaseFunction(Double mu,Double temperature){
+        return temperature*mu;
     };
     //fonction f(x)
-    public float evaluateFunction(Solution solution){
+    public Double evaluateFunction(Solution solution){
     //Le coût total d’une solution est la somme des coût de chaque Ri.
-        return 0;
+
+        return null;
     };
     //generer voisinage
     public List<Solution> generateNeighbors(){
 
+        return null;
     }
     //executer le recuit simulé
-    public Solution executeAlgo(Float tempMin,Float mu){
+    public Solution executeAlgo(Double tempMin,Double mu){
         //a initialiser
         int nb_essais=0;
         Random r = new Random();
-        float delta =0;
-        float temperatureCourante=tempInitiale;
+        Double delta = 0.0;
+        Double temperatureCourante=tempInitiale;
         Solution solutionCourante =solutionInitiale;
         while (temperatureCourante>tempMin){
                 for(int nb_etape=0;nb_etape<nb_essais;nb_etape++){
+                    //ou intervertir directement des aretes
                     List<Solution>  neighbors = generateNeighbors();
                     int nbAlea =r.nextInt();
                     Solution solutionVoisine= neighbors.get(nbAlea);
@@ -44,14 +45,18 @@ public class RecuitSimule {
 
                     }else{
                         //critère de Metropolis
+                        Double pAlea =r.nextDouble();
+                        if(pAlea<=Math.exp(-delta/temperatureCourante)){
+                            solutionCourante = solutionVoisine;
+                        }
                     }
 
 
                 }
-              temperatureCourante=decreaseFunction(mu);
+              temperatureCourante=decreaseFunction(mu,temperatureCourante);
         }
 
-        return null;
+        return solutionCourante;
     }
 
 }
