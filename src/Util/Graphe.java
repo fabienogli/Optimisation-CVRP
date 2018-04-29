8b94f6f31f88bff6a54b490115ac6501bd468e09
Mark Kpamy <markkpamy@gmail.com>
2018-04-29 13:19:32 +0200

    commit recuit simulé

--- src/Util/Graphe.java
+++ src/Util/Graphe.java
@@ -3,14 +3,25 @@ package Util;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Graphe {
 
-    private  Depot depot;
+    private Depot depot;
+    private Map<Integer, Sommet> clients;
+    private ArrayList<Arc> aretes = new ArrayList<>();
+
+    public Graphe(String dataset) {
+        this.clients = SommetFactory.getDataFromDb("data01");
+        depot = (Depot) this.clients.get(0);
+    }
+
     public static void main(String args[]) {
         Graph graph = new SingleGraph("Tutorial 1");
-        graph.addNode("A" );
-        graph.addNode("B" );
-        graph.addNode("C" );
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
         graph.addEdge("AB", "A", "B");
         graph.addEdge("BC", "B", "C");
         graph.addEdge("CA", "C", "A");
